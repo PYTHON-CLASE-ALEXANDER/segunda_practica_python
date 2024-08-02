ea866af5c2fcff3109a225e5560cd11bd66e3c6e
Caro-Villa <ccastrillon614@gmail.com>
2024-08-01 22:16:43 -0500

    Ejercicios realizados

---
+++ lista.py
@@ -0,0 +1,25 @@
+# LISTA
+
+# 1. Crea una lista de colores rojo, verde y azul. Lo deberas almacenar en una lista llamada colores
+colores = ["rojo", "verde", "azul"]
+
+# 2. Escribe el codigo que muestra los colores de la lista que acaba de crear, usando el comando print()
+print(colores)
+
+# 3. Modifica el codigo para que ahora solo se muestre el segundo elemento de la lista (verde) que has creado.
+print(colores[1])
+
+# 4. Como cambiarias el primer color rojo por el color rosa de la lista 
+colores[0] = "rosa"
+print(colores)
+
+# 5. Elimina la tercera entrada de la lista
+del colores[2]
+
+# 6. Ahora añade el color lila al final de la lista 
+colores.append("lila")
+print(colores)
+
+# 7. Ahora añade el color amarillo en la primera posicion (indice 0)
+colores.insert(0, "amarillo")
+print(colores)
